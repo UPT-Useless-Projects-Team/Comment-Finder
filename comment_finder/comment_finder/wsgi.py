1eed319c133e80408d6404d40a0b2d2eee4b2af6
juliobednaro <julek_12345@wp.pl>
2023-09-20 20:40:42 +0200

    test

--- comment_finder/comment_finder/wsgi.py
+++ comment_finder/comment_finder/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comment_finder.settings')
 
-application = get_wsgi_application()
+def application(environ, start_response):
+    environ["PATH_INFO"] = unquote(environ["PATH_INFO"]).encode('utf-8').decode('iso-8859-1')
+    _application = get_wsgi_application()
+    return _application(environ, start_response)
+
+# application = get_wsgi_application()

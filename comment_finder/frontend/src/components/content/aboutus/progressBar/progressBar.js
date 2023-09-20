387bbbb16f77d2691f3200c791ce0e2b44fa6822
LOST <62392304+LostWorld0@users.noreply.github.com>
2023-09-20 22:24:48 +0200

    hngdfuigbndfsiugh

---
+++ comment_finder/frontend/src/components/content/aboutus/progressBar/progressBar.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import "./progressBar.css";
+
+const ProgressBar = () => {
+    return(
+        <div className='progressBar'>
+            <div className='progression'>
+                
+            </div>
+        </div>
+    ) 
+}
+
+export default ProgressBar;
